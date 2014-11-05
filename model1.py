f45d8892ff2e3af095a24cb47bdcb83b7a4ec5c4
Anna Page <anna.m.page@gmail.com>
2014-11-05 14:46:06 +0000

    Indent and ancestral population growth
    Correctly indented
    Added ancestral population growth to model

--- model1.py
+++ model1.py
@@ -1,27 +1,32 @@
 import dadi
-from dadi import PhiManip
+from dadi import PhiManip, Integration
 import numpy
 
-def split((param),(n1,n2),pts):
+def parallel_domestication((nu1F,T1),(n1,n2),pts):
 
-"""
-Model with two splits, with bottlenecks in pop 2 and 3, and migration between all three populations
+    """
+    Model with two splits, with bottlenecks in pop 2 and 3, and migration between all three populations
+    
+    nu1F: ancestral population size after growth (nu=pop size 1=population 1 F=final)(initial size is defined to be 1)
+    T1: the scaled time between ancestral population growth and split 1
+    """
 
-define parameters here
-"""
+    #create grid
+    xx = yy =dadi.Numerics.default_grid(pts)
 
-#create grid
-pts=20
-xx= dadi.Numerics.default_grid(pts)
+    #phi for equilibrium ancestral population
+    phi=PhiManip.phi_1D(xx)
 
-#phi for equilibrium ancestral population
-phi=PhiManip.phi_1D(xx)
+    # Ancestral population growth
+    phi = Integration.one_pop(phi, xx, T=Tp, nu=nu1F)
 
-#first split of population 2 from 1
-phi=PhiManip.phi_1D_to_2D(xx,phi)
+    #first split of population 2 from 1
+    phi=PhiManip.phi_1D_to_2D(xx,phi)
 
-#split of population 3 from 1
-phi=PhiManip.phi_2D_to_3D_split_1(xx,phi)
+    #split of population 3 from 1
+    phi=PhiManip.phi_2D_to_3D_split_1(xx,phi)
 
+    #calculate spectrum
+    sfs = dadi.Spectrum.from_phi(phi, (n1,n2), (xx,yy))
 
-
+    return sfs
